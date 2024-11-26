80be9368a9f5e3c44df33cf4341c97564b5b5841
batuhanaltog <batuhanaltog88@gmail.com>
2024-11-26 19:26:36 +0300

    finished api's

--- chapter4-natours/app.js
+++ chapter4-natours/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-app.use(express.json());
+
+const app = express();
 
 // 1-Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('MIDDLEWARES');
   next();
 });
 
